226d1cf2966e71b8ca1d1d22b10dcd70244cd14c
KostasAnag <82450466+KostasAnag@users.noreply.github.com>
2022-11-03 13:23:04 +0200

    password generator js

---
+++ index.js
@@ -0,0 +1,122 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passwordLength = 15;
+let password1Par = document.getElementById("pswd1");
+let password2Par = document.getElementById("pswd2");
+
+function getRandomCharacter() {
+  let randomChar = Math.floor(Math.random() * characters.length);
+  return characters[randomChar];
+}
+
+function generateRandomPassword() {
+  let randomPassword = " ";
+  for (let i = 0; i < passwordLength; i++) {
+    randomPassword += getRandomCharacter();
+  }
+  return randomPassword;
+}
+
+//const generatedPasswordOne = generateRandomPassword();
+//const generatedPasswordTwo = generateRandomPassword();
+
+function showPswd() {
+  password1Par.textContent = generateRandomPassword();
+  password2Par.textContent = generateRandomPassword();
+}
+function clearPswd() {
+  password1Par.textContent = " ";
+  password2Par.textContent = " ";
+}
